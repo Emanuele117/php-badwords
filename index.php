04bffc2e220dd0147c5ec5b888ed932a2e03c80f
Emanuele117 <emanuele.pica7@gmail.com>
2021-12-01 15:15:30 +0100

    stampato paragrafo con lunghezza

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+$paragrafo_casuale = "In una terra lontana, dietro le montagne Parole, lontani dalle terre di Vocalia e Consonantia, vivono i testi casuali. Vivono isolati nella cittadina di Lettere.";
+
+var_dump(strlen($paragrafo_casuale))
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <?php echo $paragrafo_casuale  ?>  </h1>
+</body>
+</html>
